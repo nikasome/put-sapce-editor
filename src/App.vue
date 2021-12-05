11a2c3df5e05070b531f7aa7135e253aa8ac4276
市川航暉 <ichikawa@ichikawakazukinoMac-mini.local>
2021-12-05 18:32:44 +0900

    余計なものは入れない

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Editor msg="入力すると自動でスペースが入力されます。"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Editor from './components/Editor.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Editor
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #748799;
 }
 </style>
---
+++ src/components/Editor.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="editor-area">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Editor',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+.editor-area {
+  margin-top: 10px;
+}
+</style>
